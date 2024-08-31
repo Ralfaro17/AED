a8c1e7a2622c13c203951964d3f848f8195770ee
Roger <123581434+Ralfaro17@users.noreply.github.com>
2024-08-30 19:10:29 -0600

    Add CP3-arreglos

---
+++ Arreglos/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Arreglos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEstablecer = new System.Windows.Forms.Button();
+            this.txtEstablecer = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.lbImprimir = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label1.Location = new System.Drawing.Point(62, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cantidad de personas";
+            // 
+            // btnEstablecer
+            // 
+            this.btnEstablecer.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnEstablecer.Location = new System.Drawing.Point(555, 19);
+            this.btnEstablecer.Name = "btnEstablecer";
+            this.btnEstablecer.Size = new System.Drawing.Size(130, 32);
+            this.btnEstablecer.TabIndex = 1;
+            this.btnEstablecer.Text = "Establecer";
+            this.btnEstablecer.UseVisualStyleBackColor = true;
+            this.btnEstablecer.Click += new System.EventHandler(this.btnEstablecer_Click);
+            // 
+            // txtEstablecer
+            // 
+            this.txtEstablecer.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.txtEstablecer.Location = new System.Drawing.Point(341, 19);
+            this.txtEstablecer.Name = "txtEstablecer";
+            this.txtEstablecer.Size = new System.Drawing.Size(156, 32);
+            this.txtEstablecer.TabIndex = 2;
+            // 
+            // txtId
+            // 
+            this.txtId.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.txtId.Location = new System.Drawing.Point(35, 108);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(95, 32);
+            this.txtId.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label2.Location = new System.Drawing.Point(30, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 26);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label3.Location = new System.Drawing.Point(152, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 26);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Nombres";
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.txtNombres.Location = new System.Drawing.Point(157, 108);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(338, 32);
+            this.txtNombres.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label4.Location = new System.Drawing.Point(496, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 26);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Edad";
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.txtEdad.Location = new System.Drawing.Point(501, 108);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(98, 32);
+            this.txtEdad.TabIndex = 3;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnBuscar.Location = new System.Drawing.Point(629, 79);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(130, 61);
+            this.btnBuscar.TabIndex = 1;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnAgregar.Location = new System.Drawing.Point(54, 161);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(130, 45);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnBorrar.Location = new System.Drawing.Point(254, 161);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(130, 45);
+            this.btnBorrar.TabIndex = 1;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnActualizar.Location = new System.Drawing.Point(439, 161);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(130, 45);
+            this.btnActualizar.TabIndex = 1;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnImprimir.Location = new System.Drawing.Point(629, 161);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(130, 45);
+            this.btnImprimir.TabIndex = 1;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            this.btnImprimir.Click += new System.EventHandler(this.btnImprimir_Click);
+            // 
+            // lbImprimir
+            // 
+            this.lbImprimir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lbImprimir.FormattingEnabled = true;
+            this.lbImprimir.ItemHeight = 20;
+            this.lbImprimir.Location = new System.Drawing.Point(40, 235);
+            this.lbImprimir.Name = "lbImprimir";
+            this.lbImprimir.Size = new System.Drawing.Size(718, 204);
+            this.lbImprimir.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbImprimir);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtNombres);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEstablecer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnImprimir);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnEstablecer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Arreglos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEstablecer;
+        private System.Windows.Forms.TextBox txtEstablecer;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnImprimir;
+        private System.Windows.Forms.ListBox lbImprimir;
+    }
+}
+
---
+++ Arreglos/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arreglos
+{
+    public partial class Form1 : Form
+    {
+        // Declaraciones globales
+        int[] id;
+        string[] Nombres;
+        int[] Edad;
+        int N, Tam;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Limpiar()
+        {
+            txtEdad.Clear();
+            txtNombres.Clear(); 
+            txtId.Clear();
+            txtId.Focus();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (N <= Tam - 1)
+            {
+                id[N] = int.Parse(txtId.Text);
+                Nombres[N] = txtNombres.Text;
+                Edad[N] = int.Parse(txtEdad.Text);
+                MessageBox.Show("La persona con el Id " + id[N] + " Se ha registrado exitosamente");
+                N++;
+                Limpiar();
+            }
+            else
+            {
+                MessageBox.Show("No hay espacio disponible");
+            }
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(txtId.Text);
+            int i = 0;
+            while(i < N && x != id[i])
+            {
+                i++;
+            }
+
+            if (i >= N)
+            {
+                MessageBox.Show("La persona con el Id " + x + " no se ha encontrado");
+            }
+            else
+            {
+                for (int k = i; k < N - 1; k++)
+                {
+                    id[k] = id[k + 1];
+                    Nombres[k] = Nombres[k + 1];
+                    Edad[k] = Edad[k + 1];
+                }
+                N--;
+                MessageBox.Show("La persona con el id " + x + " se ha eliminado");
+                Limpiar();
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(txtId.Text);
+            int i = 0;
+            while (i < N && x != id[i])
+            {
+                i++;
+            }
+
+            if (i >= N)
+            {
+                MessageBox.Show("La persona con el Id " + x + " no se ha encontrado");
+            }
+            else
+            {
+                txtNombres.Text = Nombres[i];
+                txtEdad.Text = Edad[i].ToString();
+                MessageBox.Show("Persona con Id " + x + " encontrada");
+            }
+        }
+
+        private void btnImprimir_Click(object sender, EventArgs e)
+        {
+            lbImprimir.Items.Clear();
+            for (int i = 0; i < N; i++)
+            {
+                lbImprimir.Items.Add(id[i] + "\t" + Nombres[i] + "\t" + Edad[i]);
+            }
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(txtId.Text);
+            int i = 0;
+            while (i < N && x != id[i])
+            {
+                i++;
+            }
+
+            if (i >= N)
+            {
+                MessageBox.Show("La persona con el Id " + x + " no se ha encontrado");
+            }
+            else
+            {
+                Nombres[i] = txtNombres.Text;
+                Edad[i] = int.Parse(txtEdad.Text);
+
+                MessageBox.Show("La persona con el id " + x + " Ha sido actualizada");
+                Limpiar();
+            }
+        }
+
+        private void btnEstablecer_Click(object sender, EventArgs e)
+        {
+            Tam = int.Parse(txtEstablecer.Text);
+            id = new int[Tam];
+            Nombres = new string[Tam];
+            Edad = new int[Tam];
+            N = 0;
+
+            MessageBox.Show("Se ha establecido la cantidad de " + Tam);
+        }
+    }
+}
