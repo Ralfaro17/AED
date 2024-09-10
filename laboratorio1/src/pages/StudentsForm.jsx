15d4155e0f211c79453e670066409a0c761cf4a2
Roger <123581434+Ralfaro17@users.noreply.github.com>
2024-09-10 00:02:57 -0600

    fix typo

--- laboratorio1/src/pages/StudentsForm.jsx
+++ laboratorio1/src/pages/StudentsForm.jsx
@@ -455,7 +455,7 @@ function StudentsForm() {
               className="w-full text-wrap"
               onClick={totalAndBestStudent}
             >
-              promedio general y mejor estudiante
+              promedio general, mejor estudiante
             </Button>
             <Button
               className="w-full text-wrap p-2"
