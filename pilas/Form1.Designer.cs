98bf3f59379ecf55afa0efd2edd2c595e14869c3
Roger <123581434+Ralfaro17@users.noreply.github.com>
2024-09-25 13:22:49 -0600

    add pilas CP

---
+++ pilas/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace pilas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtElemento = new System.Windows.Forms.TextBox();
+            this.btnApilar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCopiar = new System.Windows.Forms.Button();
+            this.btnPop = new System.Windows.Forms.Button();
+            this.btnPeek = new System.Windows.Forms.Button();
+            this.lbPila = new System.Windows.Forms.ListBox();
+            this.lbCopiar = new System.Windows.Forms.ListBox();
+            this.lbImprimir = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Elemento:";
+            // 
+            // txtElemento
+            // 
+            this.txtElemento.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtElemento.Location = new System.Drawing.Point(120, 15);
+            this.txtElemento.Name = "txtElemento";
+            this.txtElemento.Size = new System.Drawing.Size(165, 26);
+            this.txtElemento.TabIndex = 1;
+            this.txtElemento.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtElemento_KeyPress);
+            // 
+            // btnApilar
+            // 
+            this.btnApilar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApilar.Location = new System.Drawing.Point(291, 13);
+            this.btnApilar.Name = "btnApilar";
+            this.btnApilar.Size = new System.Drawing.Size(130, 29);
+            this.btnApilar.TabIndex = 2;
+            this.btnApilar.Text = "Push / Apilar";
+            this.btnApilar.UseVisualStyleBackColor = true;
+            this.btnApilar.Click += new System.EventHandler(this.btnApilar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Pila original";
+            // 
+            // btnCopiar
+            // 
+            this.btnCopiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCopiar.Location = new System.Drawing.Point(189, 68);
+            this.btnCopiar.Name = "btnCopiar";
+            this.btnCopiar.Size = new System.Drawing.Size(130, 29);
+            this.btnCopiar.TabIndex = 2;
+            this.btnCopiar.Text = "Copiar";
+            this.btnCopiar.UseVisualStyleBackColor = true;
+            this.btnCopiar.Click += new System.EventHandler(this.btnCopiar_Click);
+            // 
+            // btnPop
+            // 
+            this.btnPop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPop.Location = new System.Drawing.Point(368, 68);
+            this.btnPop.Name = "btnPop";
+            this.btnPop.Size = new System.Drawing.Size(130, 29);
+            this.btnPop.TabIndex = 2;
+            this.btnPop.Text = "Pop / Desapilar";
+            this.btnPop.UseVisualStyleBackColor = true;
+            this.btnPop.Click += new System.EventHandler(this.btnPop_Click);
+            // 
+            // btnPeek
+            // 
+            this.btnPeek.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPeek.Location = new System.Drawing.Point(533, 68);
+            this.btnPeek.Name = "btnPeek";
+            this.btnPeek.Size = new System.Drawing.Size(235, 29);
+            this.btnPeek.TabIndex = 2;
+            this.btnPeek.Text = "Peek / Primero en pila";
+            this.btnPeek.UseVisualStyleBackColor = true;
+            this.btnPeek.Click += new System.EventHandler(this.btnPeek_Click);
+            // 
+            // lbPila
+            // 
+            this.lbPila.FormattingEnabled = true;
+            this.lbPila.Location = new System.Drawing.Point(11, 113);
+            this.lbPila.Name = "lbPila";
+            this.lbPila.Size = new System.Drawing.Size(154, 316);
+            this.lbPila.TabIndex = 3;
+            // 
+            // lbCopiar
+            // 
+            this.lbCopiar.FormattingEnabled = true;
+            this.lbCopiar.Location = new System.Drawing.Point(189, 113);
+            this.lbCopiar.Name = "lbCopiar";
+            this.lbCopiar.Size = new System.Drawing.Size(154, 316);
+            this.lbCopiar.TabIndex = 3;
+            // 
+            // lbImprimir
+            // 
+            this.lbImprimir.FormattingEnabled = true;
+            this.lbImprimir.Location = new System.Drawing.Point(368, 113);
+            this.lbImprimir.Name = "lbImprimir";
+            this.lbImprimir.Size = new System.Drawing.Size(400, 316);
+            this.lbImprimir.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbImprimir);
+            this.Controls.Add(this.lbCopiar);
+            this.Controls.Add(this.lbPila);
+            this.Controls.Add(this.btnPeek);
+            this.Controls.Add(this.btnPop);
+            this.Controls.Add(this.btnCopiar);
+            this.Controls.Add(this.btnApilar);
+            this.Controls.Add(this.txtElemento);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Pilas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtElemento;
+        private System.Windows.Forms.Button btnApilar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCopiar;
+        private System.Windows.Forms.Button btnPop;
+        private System.Windows.Forms.Button btnPeek;
+        private System.Windows.Forms.ListBox lbPila;
+        private System.Windows.Forms.ListBox lbCopiar;
+        private System.Windows.Forms.ListBox lbImprimir;
+    }
+}
+
---
+++ pilas/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pilas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Stack<string> PilaNombre = new Stack<string>();
+
+        void Imprimir(ListBox listBox)
+        {
+            listBox.Items.Clear();
+
+            foreach( var item in PilaNombre)
+            {
+                listBox.Items.Add(item);
+                listBox.Items.Add("-------------------");
+            }
+        }
+
+        void Agregar()
+        {
+            PilaNombre.Push(txtElemento.Text);
+            Imprimir(lbPila);
+            txtElemento.Clear();
+            txtElemento.Focus();
+
+        }
+
+        private void txtElemento_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if((Keys)e.KeyChar == Keys.Enter)
+            {
+                Agregar();
+            }
+        }
+
+        private void btnPop_Click(object sender, EventArgs e)
+        {
+            if(PilaNombre.Count == 0)
+            {
+                MessageBox.Show("No hay elementos en la pila");
+                return;
+            }
+
+            lbImprimir.Items.Add("Popping/Desapilado: " + PilaNombre.Pop());
+
+            Imprimir(lbCopiar);
+        }
+
+        private void btnApilar_Click(object sender, EventArgs e)
+        {
+            Agregar();
+        }
+
+        private void btnCopiar_Click(object sender, EventArgs e)
+        {
+            Imprimir(lbCopiar);
+        }
+
+        private void btnPeek_Click(object sender, EventArgs e)
+        {
+            if(PilaNombre.Count == 0)
+            {
+                MessageBox.Show("No hay elementos en la pila");
+                return;
+            }
+
+            lbImprimir.Items.Add("Primer elemento en pila " + PilaNombre.Peek());
+        }
+    }
+}
