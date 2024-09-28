3f41237d6ae84a9ba5f637d00a1372c6c64c44fc
Roger <123581434+Ralfaro17@users.noreply.github.com>
2024-09-27 22:03:22 -0600

    add sistematico CP (queue)

---
+++ sistematico/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace sistematico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtElemento = new System.Windows.Forms.TextBox();
+            this.btnEncolar = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDesencolar = new System.Windows.Forms.Button();
+            this.btnPeek = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbCola = new System.Windows.Forms.ListBox();
+            this.lbCopiaCola = new System.Windows.Forms.ListBox();
+            this.lbImprimir = new System.Windows.Forms.ListBox();
+            this.btnCopiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Elemento:";
+            // 
+            // txtElemento
+            // 
+            this.txtElemento.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtElemento.Location = new System.Drawing.Point(96, 19);
+            this.txtElemento.Name = "txtElemento";
+            this.txtElemento.Size = new System.Drawing.Size(442, 26);
+            this.txtElemento.TabIndex = 1;
+            this.txtElemento.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtElemento_KeyPress);
+            // 
+            // btnEncolar
+            // 
+            this.btnEncolar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEncolar.Location = new System.Drawing.Point(557, 14);
+            this.btnEncolar.Name = "btnEncolar";
+            this.btnEncolar.Size = new System.Drawing.Size(175, 37);
+            this.btnEncolar.TabIndex = 2;
+            this.btnEncolar.Text = "Enqueue / Encolar";
+            this.btnEncolar.UseVisualStyleBackColor = true;
+            this.btnEncolar.Click += new System.EventHandler(this.btnEncolar_Click);
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBuscar.Location = new System.Drawing.Point(557, 72);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(175, 26);
+            this.txtBuscar.TabIndex = 3;
+            this.txtBuscar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBuscar_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(475, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Buscar:";
+            // 
+            // btnDesencolar
+            // 
+            this.btnDesencolar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDesencolar.Location = new System.Drawing.Point(479, 123);
+            this.btnDesencolar.Name = "btnDesencolar";
+            this.btnDesencolar.Size = new System.Drawing.Size(253, 37);
+            this.btnDesencolar.TabIndex = 5;
+            this.btnDesencolar.Text = "Dequeue / Desencolar";
+            this.btnDesencolar.UseVisualStyleBackColor = true;
+            this.btnDesencolar.Click += new System.EventHandler(this.btnDesencolar_Click);
+            // 
+            // btnPeek
+            // 
+            this.btnPeek.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPeek.Location = new System.Drawing.Point(479, 177);
+            this.btnPeek.Name = "btnPeek";
+            this.btnPeek.Size = new System.Drawing.Size(253, 37);
+            this.btnPeek.TabIndex = 6;
+            this.btnPeek.Text = "Peek / Primero en la cola";
+            this.btnPeek.UseVisualStyleBackColor = true;
+            this.btnPeek.Click += new System.EventHandler(this.btnPeek_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Cola original";
+            // 
+            // lbCola
+            // 
+            this.lbCola.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCola.FormattingEnabled = true;
+            this.lbCola.ItemHeight = 20;
+            this.lbCola.Location = new System.Drawing.Point(16, 110);
+            this.lbCola.Name = "lbCola";
+            this.lbCola.Size = new System.Drawing.Size(211, 324);
+            this.lbCola.TabIndex = 8;
+            // 
+            // lbCopiaCola
+            // 
+            this.lbCopiaCola.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCopiaCola.FormattingEnabled = true;
+            this.lbCopiaCola.ItemHeight = 20;
+            this.lbCopiaCola.Location = new System.Drawing.Point(252, 110);
+            this.lbCopiaCola.Name = "lbCopiaCola";
+            this.lbCopiaCola.Size = new System.Drawing.Size(211, 324);
+            this.lbCopiaCola.TabIndex = 9;
+            // 
+            // lbImprimir
+            // 
+            this.lbImprimir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbImprimir.FormattingEnabled = true;
+            this.lbImprimir.ItemHeight = 20;
+            this.lbImprimir.Location = new System.Drawing.Point(479, 229);
+            this.lbImprimir.Name = "lbImprimir";
+            this.lbImprimir.Size = new System.Drawing.Size(253, 204);
+            this.lbImprimir.TabIndex = 11;
+            // 
+            // btnCopiar
+            // 
+            this.btnCopiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCopiar.Location = new System.Drawing.Point(252, 64);
+            this.btnCopiar.Name = "btnCopiar";
+            this.btnCopiar.Size = new System.Drawing.Size(111, 37);
+            this.btnCopiar.TabIndex = 12;
+            this.btnCopiar.Text = "Copiar";
+            this.btnCopiar.UseVisualStyleBackColor = true;
+            this.btnCopiar.Click += new System.EventHandler(this.btnCopiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCopiar);
+            this.Controls.Add(this.lbImprimir);
+            this.Controls.Add(this.lbCopiaCola);
+            this.Controls.Add(this.lbCola);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnPeek);
+            this.Controls.Add(this.btnDesencolar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.btnEncolar);
+            this.Controls.Add(this.txtElemento);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Sistematico";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtElemento;
+        private System.Windows.Forms.Button btnEncolar;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDesencolar;
+        private System.Windows.Forms.Button btnPeek;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lbCola;
+        private System.Windows.Forms.ListBox lbCopiaCola;
+        private System.Windows.Forms.ListBox lbImprimir;
+        private System.Windows.Forms.Button btnCopiar;
+    }
+}
+
---
+++ sistematico/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sistematico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Queue<string> ColaNombre = new Queue<string>();
+
+        void Imprimir(ListBox listbox)
+        {
+            listbox.Items.Clear();
+            foreach(string item in ColaNombre)
+            {
+                listbox.Items.Add(item);
+                listbox.Items.Add("-------------------");
+            }
+        }
+
+        void Agregar()
+        {
+            string elemento = txtElemento.Text;
+            ColaNombre.Enqueue(elemento);
+
+            Imprimir(lbCola);
+
+            txtElemento.Clear();
+            txtElemento.Focus();
+        }
+
+        private void btnEncolar_Click(object sender, EventArgs e)
+        {
+            Agregar();
+        }
+
+        private void btnCopiar_Click(object sender, EventArgs e)
+        {
+            Imprimir(lbCopiaCola);
+        }
+
+        private void btnDesencolar_Click(object sender, EventArgs e)
+        {
+            if(ColaNombre.Count != 0)
+            {
+                lbImprimir.Items.Add("Desencolado: " + ColaNombre.Dequeue());
+                Imprimir(lbCola);
+            }
+            else
+            {
+                MessageBox.Show("No hay elementos para borrar en la cola");
+            }
+        }
+
+        private void btnPeek_Click(object sender, EventArgs e)
+        {
+            if(ColaNombre.Count != 0)
+            {
+                lbImprimir.Items.Add("Peeked: " + ColaNombre.Peek());
+            }
+            else
+            {
+                MessageBox.Show("No hay elementos para mostrar en la cola");
+            }
+        }
+
+        private void txtElemento_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if((Keys)e.KeyChar == Keys.Enter)
+            {
+                Agregar();
+            }
+        }
+
+        private void txtBuscar_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if((Keys)e.KeyChar != Keys.Enter)
+            {
+                return;
+            }
+
+            if (ColaNombre.Contains(txtBuscar.Text))
+            {
+                MessageBox.Show("Existe el elemento");
+            }
+            else
+            {
+                MessageBox.Show("No existe el elemento");
+            }
+
+            txtBuscar.Clear();
+            txtBuscar.Focus();
+        }
+    }
+}
