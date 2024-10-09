6c9264d4ebf0fb8388e79d1aa1361faebcaac3b6
Roger <123581434+Ralfaro17@users.noreply.github.com>
2024-10-09 16:12:52 -0600

    add ordenamientoDirecto CP

---
+++ ordenacionDirecta/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace ordenacionDirecta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbMetodo = new System.Windows.Forms.ComboBox();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.txtOrden = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbNumeros = new System.Windows.Forms.ListBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(499, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(247, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Elija mètodo de ordenaciòn ";
+            // 
+            // cbMetodo
+            // 
+            this.cbMetodo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.cbMetodo.FormattingEnabled = true;
+            this.cbMetodo.Location = new System.Drawing.Point(514, 58);
+            this.cbMetodo.Name = "cbMetodo";
+            this.cbMetodo.Size = new System.Drawing.Size(202, 32);
+            this.cbMetodo.TabIndex = 1;
+            this.cbMetodo.SelectedIndexChanged += new System.EventHandler(this.cbMetodo_SelectedIndexChanged);
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.txtNumero.Location = new System.Drawing.Point(80, 24);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(111, 29);
+            this.txtNumero.TabIndex = 2;
+            this.txtNumero.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNumero_KeyPress);
+            // 
+            // txtOrden
+            // 
+            this.txtOrden.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.txtOrden.Location = new System.Drawing.Point(493, 118);
+            this.txtOrden.Name = "txtOrden";
+            this.txtOrden.Size = new System.Drawing.Size(253, 29);
+            this.txtOrden.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label2.Location = new System.Drawing.Point(45, 408);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(204, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "0 elementos insertados";
+            // 
+            // lbNumeros
+            // 
+            this.lbNumeros.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lbNumeros.FormattingEnabled = true;
+            this.lbNumeros.ItemHeight = 16;
+            this.lbNumeros.Location = new System.Drawing.Point(34, 59);
+            this.lbNumeros.Name = "lbNumeros";
+            this.lbNumeros.Size = new System.Drawing.Size(224, 340);
+            this.lbNumeros.TabIndex = 3;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.btnLimpiar.Location = new System.Drawing.Point(264, 356);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(129, 43);
+            this.btnLimpiar.TabIndex = 4;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.lbNumeros);
+            this.Controls.Add(this.txtOrden);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.cbMetodo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Ordenacion Directa";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbMetodo;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.TextBox txtOrden;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lbNumeros;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
+
---
+++ ordenacionDirecta/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ordenacionDirecta
+{
+    public partial class Form1 : Form
+    { 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int N, aux, k, i;
+        int[] Numeros;
+
+        void Imprimir()
+        {
+            for(k = 0; k < N; k++)
+            {
+                txtOrden.Text = txtOrden.Text + (Numeros[k]) + " ";
+            }
+            txtOrden.Text = txtOrden.Text + "\r\n";
+        }
+
+        public void BurbujaMenor()
+        {
+            i = 0;
+            N = 0;
+            foreach(int Elemento in lbNumeros.Items)
+            {
+                Array.Resize(ref Numeros, N + 1);
+                Numeros[N] = Elemento;
+                N++;
+            }
+            txtOrden.Text = "";
+            for(int i = 1; i < N; i++)
+            {
+                for(int j = N - 1; j > 0; j--)
+                {
+                    if (Numeros[j - 1] > Numeros[j])
+                    {
+                        aux = Numeros[j - 1];
+                        Numeros[j - 1] = Numeros[j];
+                        Numeros[j] = aux;
+                    }
+                    Imprimir();
+                }
+            }
+        }
+
+        void BurbujaMayor()
+        {
+            N = 0;
+            foreach(int Elemento in lbNumeros.Items)
+            {
+                Array.Resize(ref Numeros, N + 1);
+                Numeros[N] = Elemento;
+                N++;
+            }
+            txtOrden.Clear();
+            for( int i = N - 1; i > 0; i--)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    if(Numeros[j] > Numeros[j + 1])
+                    {
+                        aux = Numeros[j];
+                        Numeros[j] = Numeros[j + 1];
+                        Numeros[j + 1] = aux;
+                    }
+                    Imprimir();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbMetodo.Items.Add("Burburja Menor");
+            cbMetodo.Items.Add("Burburja Mayor");
+            cbMetodo.Items.Add("Burburja Con Señal");
+            cbMetodo.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void txtNumero_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 13)
+            {
+                int Num = int.Parse(txtNumero.Text);
+                lbNumeros.Items.Add(Num);
+                lbNumeros.Text = Convert.ToString(lbNumeros.Items.Count + "elementos insertados");
+                txtNumero.Text = "";
+                txtNumero.Focus();
+            }
+        }
+
+        void BurbujaSeñal()
+        {
+            N = 0;
+            foreach(int Elemento in lbNumeros.Items)
+            {
+                Array.Resize(ref Numeros, N + 1);
+                Numeros[N] = Elemento;
+                N++;
+            }
+            txtOrden.Clear();
+            i = 0;
+            bool Band = false;
+            while (i < N - 1 && Band == false)
+            {
+                Band = true;
+                for(int j = 0; j < N - 1; j++)
+                {
+                    if(Numeros[j] > Numeros[j + 1])
+                    {
+                        int aux = Numeros[j];
+                        Numeros[j] = Numeros[j + 1];
+                        Numeros[j + 1] = aux;
+                        Band = false;
+                    }
+                    Imprimir();
+                }
+                i = i + 1;
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            lbNumeros.Items.Clear();
+        }
+
+        private void cbMetodo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(cbMetodo.SelectedIndex == 0)
+            {
+                BurbujaMenor();
+            }
+            if(cbMetodo.SelectedIndex == 1)
+            {
+                BurbujaMayor();
+            }
+            if(cbMetodo.SelectedIndex == 2)
+            {
+                BurbujaSeñal();
+            }
+
+        }
+    }
+}
