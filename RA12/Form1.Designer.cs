8271c178e2202a2d409cb05b29d57a0d7b8d01bd
Roger <123581434+Ralfaro17@users.noreply.github.com>
2024-10-18 20:25:24 -0600

    add RA12 CP

---
+++ RA12/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace RA12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.dtFecha = new System.Windows.Forms.DateTimePicker();
+            this.dgImprimir = new System.Windows.Forms.DataGridView();
+            this.lbImprimir = new System.Windows.Forms.ListBox();
+            this.cbOpciones = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgImprimir)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id";
+            // 
+            // txtId
+            // 
+            this.txtId.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.txtId.Location = new System.Drawing.Point(12, 61);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 29);
+            this.txtId.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label2.Location = new System.Drawing.Point(118, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nombres";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.txtNombre.Location = new System.Drawing.Point(118, 61);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(140, 29);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label3.Location = new System.Drawing.Point(264, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 24);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Salario";
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.txtSalario.Location = new System.Drawing.Point(264, 61);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(135, 29);
+            this.txtSalario.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label4.Location = new System.Drawing.Point(405, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(161, 24);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Fecha nacimiento";
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.btnIngresar.Location = new System.Drawing.Point(701, 33);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(91, 56);
+            this.btnIngresar.TabIndex = 2;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // dtFecha
+            // 
+            this.dtFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.dtFecha.Location = new System.Drawing.Point(405, 62);
+            this.dtFecha.Name = "dtFecha";
+            this.dtFecha.Size = new System.Drawing.Size(290, 26);
+            this.dtFecha.TabIndex = 3;
+            // 
+            // dgImprimir
+            // 
+            this.dgImprimir.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgImprimir.Location = new System.Drawing.Point(12, 119);
+            this.dgImprimir.Name = "dgImprimir";
+            this.dgImprimir.Size = new System.Drawing.Size(387, 319);
+            this.dgImprimir.TabIndex = 4;
+            // 
+            // lbImprimir
+            // 
+            this.lbImprimir.FormattingEnabled = true;
+            this.lbImprimir.Location = new System.Drawing.Point(403, 159);
+            this.lbImprimir.Name = "lbImprimir";
+            this.lbImprimir.Size = new System.Drawing.Size(388, 277);
+            this.lbImprimir.TabIndex = 5;
+            // 
+            // cbOpciones
+            // 
+            this.cbOpciones.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.cbOpciones.FormattingEnabled = true;
+            this.cbOpciones.Location = new System.Drawing.Point(403, 119);
+            this.cbOpciones.Name = "cbOpciones";
+            this.cbOpciones.Size = new System.Drawing.Size(389, 32);
+            this.cbOpciones.TabIndex = 6;
+            this.cbOpciones.SelectedIndexChanged += new System.EventHandler(this.cbOpciones_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cbOpciones);
+            this.Controls.Add(this.lbImprimir);
+            this.Controls.Add(this.dgImprimir);
+            this.Controls.Add(this.dtFecha);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgImprimir)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.DateTimePicker dtFecha;
+        private System.Windows.Forms.DataGridView dgImprimir;
+        private System.Windows.Forms.ListBox lbImprimir;
+        private System.Windows.Forms.ComboBox cbOpciones;
+    }
+}
+
---
+++ RA12/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RA12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public struct Datos
+        {
+            public int Id;
+            public string Nombres;
+            public double Salario;
+            public DateTime Fecha;
+        }
+
+        Datos[] Registro;
+        int N;
+        int i = 0;
+        
+        void Imprimir()
+        {
+            dgImprimir.Rows.Clear();
+            N = 0;
+            foreach(var Elemento in Registro)
+            {
+                dgImprimir.Rows.Add();
+                dgImprimir.Rows[N].Cells[0].Value = Elemento.Id;
+                dgImprimir.Rows[N].Cells[1].Value = Elemento.Nombres;
+                dgImprimir.Rows[N].Cells[2].Value = Elemento.Salario;
+                dgImprimir.Rows[N].Cells[3].Value = Elemento.Fecha;
+                N++;
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgImprimir.Columns.Add("", "Id");
+            dgImprimir.Columns.Add("", "Nombres");
+            dgImprimir.Columns.Add("", "Salario");
+            dgImprimir.Columns.Add("", "Fecha");
+
+            cbOpciones.Items.Add("Promedio");
+            cbOpciones.Items.Add("Suma de salario");
+            cbOpciones.Items.Add("Salario mayor de 20");
+            cbOpciones.Items.Add("Despues de una Fecha");
+            cbOpciones.Items.Add("Edades");
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            Array.Resize(ref Registro, i + 1);
+            Registro[i].Id = int.Parse(txtId.Text);
+            Registro[i].Nombres = txtNombre.Text;
+            Registro[i].Salario = double.Parse(txtSalario.Text);
+            Registro[i].Fecha = DateTime.Parse(dtFecha.Text);
+            i++;
+            Imprimir();
+        }
+
+        private void cbOpciones_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch(cbOpciones.SelectedIndex) 
+            {
+                case 0:
+                    var Consulta1 = Registro.Average(x => x.Salario);
+                    lbImprimir.DataSource = null;
+                    lbImprimir.Items.Add("Promedio de salario: " + Consulta1);
+                    break;
+                case 1:
+                    var Consulta2 = Registro.Sum(x => x.Salario);
+                    lbImprimir.DataSource = null;
+                    lbImprimir.Items.Add("Suma de salario: " + Consulta2);
+                    break;
+                case 2:
+                    var Consulta3 = Registro.Where(x => x.Salario > Registro.Average(c => c.Salario)).Select(x => x.Nombres);
+                    lbImprimir.DataSource = null;
+                    lbImprimir.DataSource = Consulta3.ToList();
+                    break;
+                case 3:
+                    var Consulta4 = Registro.Where(x => x.Fecha > DateTime.Parse("26/04/1990")).Select(x => x.Nombres);
+                    lbImprimir.DataSource = null;
+                    lbImprimir.DataSource = Consulta4.ToList();
+                    break;
+                case 4:
+                    var Consulta5 = from a in Registro select DateTime.Now.Year - a.Fecha.Year;
+                    lbImprimir.DataSource = null;
+                    lbImprimir.DataSource = Consulta5.ToList();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
